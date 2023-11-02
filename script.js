1ae71800ccac33a64623d91c79418c6b9ce3dcc9
Elizabeth Kershaw <lizzykershaw99@hotmail.com>
2023-11-02 16:57:46 +1100

    pics added in a table

--- script.js
+++ script.js
@@ -1,58 +1,17 @@
-//this section will go in the journal to allow users to log where they have been
-document.addEventListener('DOMContentLoaded', function () {
-    const dataForm = document.getElementById('dataForm');
-    const outputDiv = document.getElementById('output');
-
-    let countryData = {
-        countryNames: [],
-        cityNames: [],
-        townNames: []
-    };
-
-    // Function to update the display with the data
-    function updateOutput() {
-        outputDiv.textContent = JSON.stringify(countryData, null, 2);
+const tableRow = (text) => {
+    let tableRows = document.createElement('td')
+    let tr = document.querySelector('table').appendChild(tableRows)
+    for(let i=0; i<=4;i++){
+        return tableRows[i]
     }
+    tr.innerHTML(text)
+}
+onclick = tableRow
 
-    updateOutput();
-
-    dataForm.addEventListener('submit', function (event) {
-        event.preventDefault();
-
-        const countriesInput = document.getElementById('countries');
-        const citiesInput = document.getElementById('cities');
-        const townsInput = document.getElementById('towns');
-
-        const newCountries = countriesInput.value.split(',').map(country => country.trim());
-        const newCities = citiesInput.value.split(',').map(city => city.trim());
-        const newTowns = townsInput.value.split(',').map(town => town.trim());
-
-        // Append new data to the existing data
-        countryData.countryNames = countryData.countryNames.concat(newCountries);
-        countryData.cityNames = countryData.cityNames.concat(newCities);
-        countryData.townNames = countryData.townNames.concat(newTowns);
-
-        // Clear the input fields
-        countriesInput.value = '';
-        citiesInput.value = '';
-        townsInput.value = '';
-
-        // Update the display with the combined data
-        updateOutput();
-    });
-});
-
-
-//console.log(placesVisited('Australi', 'Sydney','lane cove'));
-let equipmentData;
-
-const adventurePrep = (activity, equipment, food) => {
-    equipmentData = {
-        activityName: activity,
-        equipmentName: equipment,
-        foodName: food
-    }
-    return `Today I am going on a ${activity} adventure. I will need to pack ${equipment} and ${food}.`
+const tableColumn = (text) => {
+    let tableColumns = document.createElement('tr');
+    let tc = document.querySelector('table').appendChild(tableColumns)
+    tc.innerHTML(text)
 }
 
-//console.log(adventurePrep('camping', ['tent', 'shoes', 'map'], ['water','pasta']));
+//onclick = tableColumn
