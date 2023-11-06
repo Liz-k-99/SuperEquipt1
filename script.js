455420fd7603bd3d5fe015bd142f891ebd8dc999
Elizabeth Kershaw <lizzykershaw99@hotmail.com>
2023-11-06 22:31:40 +1100

    form added

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const tableRow = (text) => {
-    let tableRows = document.createElement('td')
-    let tr = document.querySelector('table').appendChild(tableRows)
-    for(let i=0; i<=4;i++){
-        return tableRows[i]
-    }
-    tr.innerHTML(text)
-}
-onclick = tableRow
+// const tableRow = (text) => {
+//     let tableRows = document.createElement('td')
+//     let tr = document.querySelector('table').appendChild(tableRows)
+//     for(let i=0; i<=4;i++){
+//         return tableRows[i]
+//     }
+//     tr.innerHTML(text)
+// }
+// onclick = tableRow
 
 const tableColumn = (text) => {
     let tableColumns = document.createElement('tr');
